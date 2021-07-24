111a0f7bcd4ac4707e05b37e91b412349bd7cdd7
root <root@cloud01.arcominf.local>
2021-07-24 17:03:16 -0300

    Adicionado daemon

--- startdocker.sh
+++ startdocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t dalraf/ubuntu-desktop-lxde-vnc .
-docker run -v $(pwd):/srv/youtube-viewer -e USER=doro -e PASSWORD=123456 -p 6080:80 -v /dev/shm:/dev/shm dalraf/ubuntu-desktop-lxde-vnc
+docker run -d -v $(pwd):/srv/youtube-viewer -e USER=doro -e PASSWORD=123456 -p 6080:80 -v /dev/shm:/dev/shm dalraf/ubuntu-desktop-lxde-vnc
